b27e3c0fe20ec6d8a963d375d13ba7de399eb875
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2024-10-08 23:09:36 +0000

    Update docstring

--- eo_tides/__init__.py
+++ eo_tides/__init__.py
@@ -4,17 +4,26 @@ eo_tides
 
 Tide modelling tools for large-scale satellite earth observation analysis.
 
-`eo-tides` provides powerful parallelized tools for integrating satellite Earth observation data with tide modelling. `eo-tides` combines advanced tide modelling functionality from the `pyTMD` package with `pandas`, `xarray` and `odc-geo`, providing a suite of flexible tools for efficient analysis of coastal and ocean Earth observation data – from regional, continental, to global scale.
+`eo-tides` provides powerful parallelized tools for integrating satellite
+Earth observation data with tide modelling. `eo-tides` combines advanced
+tide modelling functionality from the `pyTMD` package with `pandas`,
+`xarray` and `odc-geo`, providing a suite of flexible tools for efficient
+analysis of coastal and ocean Earth observation data – from regional,
+continental, to global scale.
 
-These tools can be applied to petabytes of freely available satellite data (e.g. from Digital Earth Australia or Microsoft Planetary Computer) loaded via Open Data Cube's `odc-stac` or `datacube` packages, supporting coastal and ocean earth observation analysis for any time period or location globally.
+These tools can be applied to petabytes of freely available satellite
+data (e.g. from Digital Earth Australia or Microsoft Planetary Computer)
+loaded via Open Data Cube's `odc-stac` or `datacube` packages, supporting
+coastal and ocean earth observation analysis for any time period or
+location globally.
 
 Modules
 -------
-model : Core tide modeling functionality
-eo : Combining satellite EO data with tide modelling
-stats : Calculating statistics describing local tide dynamics and satellite observation biases
+model : Core tide modelling functionality
+eo : Combine satellite EO data with tide modelling
+stats : Calculate local tide dynamics and satellite bias statistics
 utils : Utility functions and helper tools
-validation : Loading observed tide gauge data to validate modelled tides
+validation : Load observed tide gauge data to validate modelled tides
 """
 
 # Import commonly used functions for convenience
