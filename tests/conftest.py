b58e0b7f42e05743a6c1eb4758fbf81845f27b0e
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2025-02-20 06:55:39 +0000

    Fix output path

--- tests/conftest.py
+++ tests/conftest.py
@@ -105,7 +105,7 @@ def satellite_ds(satellite_ds_load):
 # Run once per session to generate symethic HAMTIDE11 files; autouse=True
 # allows this to run without being specifically called in tests
 @pytest.fixture(scope="session", autouse=True)
-def create_synthetic_model(base_dir="data/tide_models_synthetic"):
+def create_synthetic_model(base_dir="tests/data/tide_models_synthetic"):
     """
     Generates and exports synthetic HAMTIDE11 model data
     to test clipping functionality.
